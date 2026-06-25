b96aae3e7e68e4b3575fdd6cb056d2fddd9f2019
JuanBerro-back <jdbpberroteran@gmail.com>
2026-06-25 02:24:30 -0500

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,8 +1,10 @@
-export default {
-  schema: "./src/schema/index.ts",
-  out: "./drizzle", // <--- ¡Añade esto! Le dice a Drizzle dónde buscar los archivos .sql
-  driver: 'pg',
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "../src/schema/index.ts", // Asegúrate de que el camino relativo sea correcto
+  out: "./drizzle",
+  dialect: "postgresql", // O 'pg' dependiendo de tu versión de kit
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: "postgresql://postgres:Juanchotopocho@db.dfwaopkbjcqwlzwmqbwx.supabase.co:5432/postgres",
   },
-}
+});
