74a477738e78eee5dea4c2519c61b18b03f854b9
JuanBerro-back <jdbpberroteran@gmail.com>
2026-06-25 02:09:57 -0500

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,5 +1,6 @@
+// En lib/db/drizzle.config.ts
 export default {
-  schema: "../src/schema/index.ts", // Apunta directamente al archivo que exporta las tablas
+  schema: "../../src/schema/index.ts", // <--- Nota los TRES puntos (../../)
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
